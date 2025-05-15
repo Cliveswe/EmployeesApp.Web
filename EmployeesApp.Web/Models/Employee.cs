986cfc062db019883afcb9a055ea4bd0a8fdafd2
Clive <Cliveswe@users.noreply.github.com>
2025-05-15 11:12:11 +0200

    Added the model to the project. Added taghelpers.

---
+++ EmployeesApp.Web/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesApp.Web.Models;
+
+public class Employee
+{
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Enter Name")]
+    [Display(Name = "User name", Prompt = "Enter a user name")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Enter Email")]
+    [Display(Name = "User email", Prompt = "Enter an Email")]
+    [EmailAddress(ErrorMessage = "Enter an Email")]
+    public string Email { get; set; }
+}
--- EmployeesApp.Web/Program.cs
+++ EmployeesApp.Web/Program.cs
@@ -4,12 +4,10 @@ public class Program
 {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        builder.Services.AddControllersWithViews();
 
-        app.MapGet("/", () => "Hello World!");
-        //Hej fråan Philippe
+        var app = builder.Build();
 
         app.Run();
     }
 }
-//Hej from Ghassan

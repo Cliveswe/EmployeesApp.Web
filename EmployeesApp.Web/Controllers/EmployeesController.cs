148639b4316c4468b159a2138b192b80d16a4ac6
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-05-15 11:42:56 +0200

    feat(EmployeesController) : Create controller and  Implement HttpGet method

---
+++ EmployeesApp.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using EmployeesApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeesApp.Web.Controllers;
+
+public class EmployeesController : Controller
+{
+    static EmployeeService EmployeeService = new EmployeeService();
+
+
+    [HttpGet("")]
+    public IActionResult Index()
+    { 
+        var model = EmployeeService.GetAll();
+        return View(model);
+    }
+
+}
